4c2ca9c328760f4e5ca83a4a015921947934e75c
Aakashdeep Sil <32011370+aakashdeepsil@users.noreply.github.com>
2020-09-22 13:11:04 +0530

    Add files via upload

---
+++ Lab Assignment 1/findRecords.js
@@ -0,0 +1,6 @@
+db.Student.find({StudentName:'Aakashdeep Sil'}).pretty()
+db.Faculty.find({FacultyName:'Aditya Kumar'}).pretty()
+db.COE.find({COEName:'Adhyayan Panwar'}).pretty()
+db.Admission.find({AdmissionName:'Rounak Kedia'}).pretty()
+db.Library.find({Branch:'CSE'}).pretty()
+db.CollegeFestival.find({Month:'April'}).pretty()
